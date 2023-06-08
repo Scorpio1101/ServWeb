58076e7c85541a576222f8c1d25f0e841dd5648b
framirezr915 <framirezr915@ulacit.ed.cr>
2023-06-08 15:58:12 -0600

    Cambios
    Login, PrincipalAdmin, PrincipalUsuarios

---
+++ SCRIPTS/main.js
@@ -0,0 +1,24 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+import { auth, db } from "./firebase.js";
+import { loginCheck } from "./loginCheck.js";
+
+import './signupForm.js'
+import './signinForm.js'
+import './googleLogin.js'
+import './Logout.js'
+
+// list for auth state changes
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    loginCheck(user);
+    try {
+      const querySnapshot = await getDocs(collection(db, "foros"));
+      setupPosts(querySnapshot.docs);
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    loginCheck(user);
+  }
+});
